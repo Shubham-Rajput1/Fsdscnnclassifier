689669693aae0887737d3b2d7ad195dc9c2ed3a9
Shubham-Rajput1 <shubhamrana0116@gmail.com>
2023-10-05 07:51:21 +0530

    Updated setup file

---
+++ setup.py
@@ -0,0 +1,27 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as f:
+    long_description = f.read()
+
+__version__ = "0.0.1"
+
+REPO_NAME = "Fsdscnnclassifier"
+AUTHOR_USER_NAME = "Shubham Rajput"
+SRC_REPO = "CNNClassifier"
+AUTHOR_EMAIL = "shubhamrana0116@gmail.com"
+
+setuptools.setup(
+    name=SRC_REPO,
+    version=__version__,
+    author=AUTHOR_USER_NAME,
+    author_email=AUTHOR_EMAIL,
+    description="this is my classification project",
+    long_description=long_description,
+    long_description_content="text/markdown",
+    url=f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}",
+    project_urls={
+        "Bug Tracker": f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}/issues",
+    },
+    package_dir={"": "src"},
+    packages=setuptools.find_packages(where="src")
+)
