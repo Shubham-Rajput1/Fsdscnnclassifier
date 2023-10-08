ecf5cd5f98704bc23a14c422ee3f5d4d524b6e6a
Shubham-Rajput1 <shubhamrana0116@gmail.com>
2023-10-08 12:44:57 +0530

    completing my first component

---
+++ src/CNNClassifier/components/stage_01_data_ingestion.py
@@ -0,0 +1,24 @@
+import os
+import urllib.request as request
+from zipfile import ZipFile
+from CNNClassifier import logger
+from pathlib import Path
+from tqdm import tqdm
+from CNNClassifier.entity import DataIngestionConfig
+from CNNClassifier.utils import utils
+
+class DataIngestion:
+    def __init__(self,config:DataIngestionConfig):
+        pass
+
+    def download_file(self):
+        pass
+            
+    def _get_updated_list_of_files(self):
+        pass
+    
+    def _preprocess(self):
+        pass
+
+    def unzip_and_clean(self):
+        pass
